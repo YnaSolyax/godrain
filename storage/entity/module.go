26f98cb172b549f11592dd01b60a0969c02ed502
YnaSolyax <tired0mail.ru>
2026-02-13 17:15:46 +0300

    f

--- storage/entity/module.go
+++ storage/entity/module.go
@@ -8,7 +8,6 @@ type Incident struct {
 	gorm.Model
 	Description string
 	Source      string
-	Logs        []LogItem `gorm:"foreignKey:IncidentID"`
 }
 
 type Defect struct {
@@ -27,3 +26,22 @@ type LogItem struct {
 	Content    string
 	Vector     []byte
 }
+
+/*
+table1 -incident
+ID       desc source
+...      ..  BGL.log
+
+table2 - defectss
+ID_logs        desc solution
+logs1 ... 100  ...   "упал сервер"
+при том хрнаить лог как json
+
+неизвестная длина для сравнения
+пример в базе 100, в поступившем 1000
+
+vector - отдельная сущность как суммарный контент всех логов
+на уровне дефекта
+
+переделать алгоритм вектора и использовать библиотеки bgVector
+*/
